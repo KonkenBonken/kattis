7f90cf20ef042871c37a8218ba5452e55160966f
KonkenBonken <konrad05pettersson@gmail.com>
2022-11-27 02:29:43 +0100

    Works for samples, not for 3/9 tests

--- kattriket.py
+++ kattriket.py
@@ -4,13 +4,41 @@ Roads = [tuple(int(n) for n in input().split()) for _ in range(N - 1)]
 
 for a, b in Roads:
     States[a].append(b)
+    # States[b].append(a)
+
+res = [1]
 
 
 def branch(roads):
-    path = {}
+    path = []
     for state in roads:
         if len(States[state]) > 0:
-            path[state] = branch(States[state])
+            path.append(branch(States[state]))
         else:
-            path[state] = None
+            path.append(None)
     return path
+
+
+tree = branch(States[0])
+levels = [len(tree)]
+
+
+def level(tree):
+    levels.append(sum(len(obj) for obj in tree if isinstance(obj, list)))
+    for branch in tree:
+        if isinstance(branch, list):
+            level(branch)
+
+
+level(tree)
+del levels[levels.index(0):]
+
+n = 1
+for l in levels:
+    res.append(n)
+    for _ in range(l-1):
+        n += 1
+        res.append(n)
+
+del res[-1]
+print(' ' .join(map(str, res)))
