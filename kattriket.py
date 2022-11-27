c2b456f87a8fd0956ffc572cfee5b7bba5e41874
KonkenBonken <konrad05pettersson@gmail.com>
2022-11-27 20:49:47 +0100

    Unsuccessful try

--- kattriket.py
+++ kattriket.py
@@ -2,23 +2,26 @@ N = int(input())
 States = [[] for _ in range(N)]
 Roads = [tuple(int(n) for n in input().split()) for _ in range(N - 1)]
 
-for road in Roads:
-    States[min(road)].append(max(road))
+for a, b in Roads:
+    States[a].append(b)
+    States[b].append(a)
 
 res = [1]
+if len(States[0]) % 2 == 1:
+    res.append(1)
 
 
-def branch(roads):
+def branch(roads, prev):
     path = []
     for state in roads:
-        if len(States[state]) > 0:
-            path.append(branch(States[state]))
+        if (state not in prev) and len(States[state]) > 0:
+            path.append(branch(States[state], (*prev, state)))
         else:
             path.append(None)
     return path
 
 
-tree = branch(States[0])
+tree = branch(States[0], (0,))
 levels = [len(tree)]
 
 
@@ -30,14 +33,15 @@ def level(tree):
 
 
 level(tree)
-del levels[levels.index(0):]
 
 n = 1
 for l in levels:
+    if len(res) >= N-1:
+        break
     res.append(n)
     for _ in range(l-1):
         n += 1
         res.append(n)
 
-del res[-1]
+del res[N-1:]
 print(' ' .join(map(str, res)))
