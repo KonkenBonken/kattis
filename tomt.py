7c5920a5fbb55ff68216ce4303b86c50bfc6502f
KonkenBonken <konrad05pettersson@gmail.com>
2022-11-27 18:09:13 +0100

    Tomt

--- tomt.py
+++ tomt.py
@@ -2,27 +2,33 @@ from time import time
 from random import shuffle
 start_time = time()
 
-T = int(input())
+input()
 N = int(input())
 
 houses = [(*(int(n) for n in input().split()), i) for i in range(N)]
 
 
+def dist_houses(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
 def dist(houses):
-    res = 0
-    for i in range(len(houses) - 1):
-        res += abs(houses[i][0] - houses[i + 1][0]) + \
-            abs(houses[i][1] - houses[i + 1][1])
-    return res
+    return sum(dist_houses(houses[i], houses[i+1]) for i in range(len(houses) - 1))
 
 
-current = (1e9, [])
+current = (1e9, [(0, 0, i+1) for i in range(N//2)])
 
 while time() - 11.9 < start_time:
     shuffle(houses)
-    d = dist(houses[:N//2])
-    if d < current[0]:
-        current = (d, houses[:N//2])
+    for start in range(N//2):
+        d = dist(houses[start:N//2+start])
+        if d < current[0]:
+            current = (d, houses[start:N//2+start])
+    houses.reverse()
+    for start in range(N//2):
+        d = dist(houses[start:N//2+start])
+        if d < current[0]:
+            current = (d, houses[start:N//2+start])
 
 
 print(" ".join(str(house[2]) for house in current[1]))
