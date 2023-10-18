13719e5a9953eee0bfceea349d23fa5cb8d7320f
KonkenBonken <konrad05pettersson@gmail.com>
2023-10-18 22:55:02 +0200

    meet in middle

--- logo2/script.py
+++ logo2/script.py
@@ -4,22 +4,16 @@ test_count = int(input())
 command_count = int(input())
 raw_commands = [input().split() for _ in range(command_count)]
 
-commands = []
-missing = None
+missing_index = next(i for i in range(command_count) if raw_commands[i][-1] == '?')
 
-for cmd, amt in raw_commands:
-    if amt == '?':
-        missing = cmd
-        if missing[1] == 't':
-            raise 'turn'
-    else:
-        commands.append((cmd, int(amt)))
+missing_command = raw_commands[missing_index][0]
+first_half = [(cmd,int(amt)) for cmd,amt in raw_commands[:missing_index]]
+second_half = [(cmd,int(amt)) for cmd,amt in raw_commands[missing_index + 1:]]
 
-print(commands, missing)
 
-x, y, θ = 0, 0, 0
+fx, fy, θ = 0, 0, 0
 
-for cmd, amt in commands:
+for cmd, amt in first_half:
     if cmd[1] == 't':
         if cmd[0] == 'l':
             amt *= -1
@@ -27,8 +21,22 @@ for cmd, amt in commands:
     else:
         if cmd[0] == 'b':
             amt *= -1
-        x += cos(θ) * amt
-        y += sin(θ) * amt
-    print(x, y, θ)
+        fx += cos(θ) * amt
+        fy += sin(θ) * amt
+print(fx, fy, θ)
 
-print(sqrt(x**2 + y**2))
+sx, sy, θ = 0, 0, 0
+
+for cmd, amt in reversed(second_half):
+    if cmd[1] == 't':
+        if cmd[0] == 'r':
+            amt *= -1
+        θ = (θ + amt) % 360
+    else:
+        if cmd[0] == 'f':
+            amt *= -1
+        sx += cos(θ) * amt
+        sy += sin(θ) * amt
+print(sx, sy, θ)
+
+print(sqrt((fx-sx)**2+(fy-sy)**2))
