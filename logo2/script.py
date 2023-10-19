6efb426a4dcbbdc8796c9c91b3f7f06490ecc84d
KonkenBonken <konrad05pettersson@gmail.com>
2023-10-19 17:58:16 +0200

    Added support for `bk`

--- logo2/script.py
+++ logo2/script.py
@@ -26,7 +26,6 @@ for cmd, amt in first_half:
             amt *= -1
         fx += cos(fθ) * amt
         fy += sin(fθ) * amt
-print(fx, fy, fθ)
 
 sx, sy, sθ = 0, 0, fθ
 
@@ -40,18 +39,17 @@ for cmd, amt in second_half:
             amt *= -1
         sx += cos(sθ) * amt
         sy += sin(sθ) * amt
-print(sx, sy, sθ)
 
 if missing_command[1] != 't':
-    step = 0
+    m = -1 if missing_command[0] == 'b' else 1
+    step = 1
     while True:
-        fx += cos(fθ)
-        fy += sin(fθ)
+        fx += cos(fθ)*m
+        fy += sin(fθ)*m
 
         rx = fx + sx
         ry = fy + sy
-
-        print(step, rx, ry)
+        
         if rx < .005 or ry < .005:
             print(step)
             break
