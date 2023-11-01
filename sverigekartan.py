3e6d37464dcfe24db7a1b9473a598508b048354a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-01 18:49:09 +0100

    solve group 3

--- sverigekartan.py
+++ sverigekartan.py
@@ -1,9 +1,21 @@
-# Group 2:
+# Group 2 & 3:
 R = int(input())  # = 1
 C = int(input())
-U = int(input())  # = 0
+U = int(input())
 
-karta = list(input() for _ in range(R))[0]
-changes = (((int(x), int(y)) for y, x in input().split()) for _ in range(R))
+karta = next(input() for _ in range(R))
 
-print(len(next(seq for seq in karta.split('.') if 'S' in seq)))
+changes = ((int(x) for x in input().split()) for _ in range(U))
+
+tiles = list(karta)
+
+
+def print_area():
+    print(len(next(seq for seq in "".join(tiles).split('.') if 'S' in seq)))
+
+
+print_area()
+
+for _, x in changes:
+    tiles[x-1] = '#'
+    print_area()
