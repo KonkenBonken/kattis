c8306f4116ea49a7d7fb15cf7cfb9744b938ec8a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-04 02:50:32 +0100

    print two names every iteration

--- namegeneration.py
+++ namegeneration.py
@@ -1,8 +1,12 @@
 k,i,n=lambda n:((b:=n&(n>>1))>>1)&b!=0,599184,int(input())
-q,r,h=i+n,range(20),lambda n:k(n)or k(int(format(n,'b').replace(*'1x').replace(*'01').replace(*'x0'),2))
+q,h=i+n//2,lambda n:k(n)or k(int(format(n,'b').replace(*'1x').replace(*'01').replace(*'x0'),2))
+if n%2:
+    print('abc')
 while i<q:
     i+=1
     if h(i):
         q+=1
     else:
-        print(''.join('ab'[(i>>j)&1]for j in r))
+        l=''.join('ab'[(i>>j)&1]for j in range(20))
+        print(l)
+        print(l.replace(*'ae'))
