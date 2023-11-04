18520176d0516469275b02942a5c39b2d43abe4b
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-04 02:36:32 +0100

    shorten code

--- namegeneration.py
+++ namegeneration.py
@@ -1,37 +1,8 @@
-N = int(input())
-names = set()
-
-vw = ('a', 'e', 'i', 'o', 'u')
-cn = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
-      'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
-
-
-def has_3_1(n):
-    a = n >> 1
-    b = n & a
-    c = b >> 1
-    return c & b != 0
-
-
-def has_3(n):
-    return has_3_1(n) or has_3_1(int("{0:b}".format(n).replace('1', 'x').replace('0', '1').replace('x', '0'), 2))
-
-
-def gen():
-    i = 599186
-    while True:
-        i += 1
-        if has_3(i):
-            continue
-        for c in range(len(cn)):
-            name = ''
-            for j in range(20):
-                charset = cn if i & (2**j) else vw
-                name += charset[c % len(charset)]
-            yield name
-
-
-names = gen()
-
-for _ in range(N):
-    print(next(names))
+k,i,n=lambda n:((b:=n&(n>>1))>>1)&b!=0,599184,int(input())
+q,r,h=i+n,range(20),lambda n:k(n)or k(int(format(n,'b').replace(*'1x').replace(*'01').replace(*'x0'),2))
+while i<q:
+    i+=1
+    if h(i):
+        q+=1
+    else:
+        print(''.join('ab'[(i>>j)&1]for j in r))
