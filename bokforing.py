8daa7aecb2cd6cb1903c348e91881d7475ca900a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-05 00:21:53 +0100

    track everyone

--- bokforing.py
+++ bokforing.py
@@ -1,4 +1,13 @@
-N,Q = (int(n) for n in input().split())
+N, Q = (int(n) for n in input().split())
+people = list(0 for _ in range(N))
 
-print(N)
-print(Q)
+for _ in range(Q):
+    instruction, *n = input().split()
+
+    if instruction == 'SET':
+        i, x = (int(m) for m in n)
+        people[i-1] = x
+    elif instruction == 'RESTART':
+        people = list(int(n[0]) for _ in range(N))
+    elif instruction == 'PRINT':
+        print(people[int(n[0])-1])
