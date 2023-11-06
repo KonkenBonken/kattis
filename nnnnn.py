8ad7c64e7e51a92610a7ebdd1f972c5bd0e76afd
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-06 20:40:35 +0100

    slow solve using binary search

--- nnnnn.py
+++ nnnnn.py
@@ -1,7 +1,27 @@
 L = int(input())
 
-for n in range(L+1):
-    en = n * len(str(n))
-    if en == L:
+def en(n):
+    return n * len(str(n))
+
+n = 2**20
+e=-1
+
+for _ in range(50):
+    e = en(n)
+    if e == L:
         print(n)
         break
+    elif e > L:
+        n //= 2
+    elif e < L:
+        n = n + n//2
+else:
+    while True:
+        e = en(n)
+        if e == L:
+            print(n)
+            break
+        elif e > L:
+            n -= 1
+        elif e < L:
+            n += 1
