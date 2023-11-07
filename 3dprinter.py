771d941fa324b82a590472652494416d7bba0735
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-07 20:03:48 +0100

    working for sample cases

--- 3dprinter.py
+++ 3dprinter.py
@@ -1,8 +1,8 @@
 N = int(input())
 
 def D(statues,printers,days):
-    if statues == N:
-        return days
+    if statues+printers >= N:
+        return days+1
     
     return min(D(statues+(printers-new_printers),printers+new_printers,days+1) for new_printers in range(printers+1))
     
