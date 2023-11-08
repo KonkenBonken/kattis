b9c8ff0a7081d5dcc6db76aad369c7972a80538c
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 14:19:01 +0100

    iterate through bit length

--- remorse.py
+++ remorse.py
@@ -1,15 +1,16 @@
 src = tuple(c.upper() for c in input() if c.isalnum())
-chars = tuple(reversed(sorted(set(src), key=src.count)))
+chars = sorted(set(src), key=src.count)
 
-b, i, res = 0, 0, -3
+res = -2
+l = 0
 
-while i < len(chars):
-    m = format(b, 'b')
-    res += (3 + m.count('0') + m.count('1')*3 + len(m)-1) * src.count(chars[i])
-    if m.count('0') == 0:
-        i += 1
-        res += (3 + len(m)+1 + len(m)) * src.count(chars[i])
-    b += 1
-    i += 1
-
-print(res)
+while True:
+    for b in range(2**l - 1):
+        m = format(b, 'b').rjust(l, '0')
+        char = m.count('0') + m.count('1')*3 + l-1
+        char *= src.count(chars.pop())
+        res += 3 + char
+        if len(chars) == 0:
+            print(res)
+            exit()
+    l += 1
