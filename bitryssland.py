53acd56b88a63f81e7170acc896116a233b5e88b
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 22:16:17 +0100

    count products and coins separately

--- bitryssland.py
+++ bitryssland.py
@@ -1,8 +1,20 @@
 input()
-coins = (int(x) for x in input().split())
+coins = dict((1 << i, int(x))
+             for i, x in enumerate(input().split()))
 products = (int(x) for x in input().split())
 
-balance = sum(count << i for i, count in enumerate(coins))
-price = sum(products)
+reversed_keys = list(reversed(coins.keys()))
 
-print('ja' if balance-price == 0 else 'nej')
+for price in products:
+    for value in reversed_keys:
+        if value == 0:
+            continue
+        div, price = divmod(price, value)
+        coins[value] -= div
+
+for value in reversed_keys[:-1]:
+    while coins[value] < 0:
+        coins[value >> 1] -= 1
+        coins[value] += 2
+
+print('nej' if any(x < 0 for x in coins.values()) else 'ja')
