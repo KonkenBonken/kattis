d7555b29afd2670cace3f0bdf795f320f30f77fe
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 18:06:35 +0100

    slow working solution

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,17 @@ def F(n):
     if n <= 1:
         return str(n)
     return F(n-1) + F(n-2)
+
+
+i = 1
+while True:
+    try:
+        n, p = int(input()), input()
+        f, o = F(n), 0
+        for c in range(len(f) - len(p) + 1):
+            if f[c:c+len(p)] == p:
+                o += 1
+        print(f'Case {i}: {o}')
+        i += 1
+    except EOFError:
+        break
