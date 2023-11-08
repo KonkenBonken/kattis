e4b981efdb945594843c8cb392563fd34812f791
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 21:09:11 +0100

    solution by tracking balance

---
+++ bitryssland.py
@@ -0,0 +1,10 @@
+N, M = (int(x)for x in input().split())
+coins = (int(x)for x in input().split())
+products = (int(x)for x in input().split())
+
+balance = sum(count * 2**i for i, count in enumerate(coins))
+
+for price in products:
+    balance -= price
+else:
+    print('ja' if balance == 0 else 'nej')
