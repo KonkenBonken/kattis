422ae6e3a3358fb9213df9f8c49c487a74fe8098
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 22:25:37 +0100

    compare substring bitwise instead of on strings

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,20 @@
 def F(n):
     if n <= 1:
-        return str(n)
+        return n
     a, b = 1, 2
     for _ in range(n-2):
         c = b << a.bit_length() | a
         a, b = b, c
-    return format(b, 'b')
+    return b
 
 
 i = 1
 while True:
     try:
-        n, p = int(input()), input()
+        n, p = int(input()), int(input(), 2)
         f, o = F(n), 0
-        for c in range(len(f) - len(p) + 1):
-            if f[c:c+len(p)] == p:
+        for c in range(f.bit_length() - p.bit_length() + 1):
+            if f & p << c == p << c:
                 o += 1
         print(f'Case {i}: {o}')
         i += 1
