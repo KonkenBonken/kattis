5316196634dc23f42c570c38a39458e2fbafa720
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 21:12:36 +0100

    calculate total price

--- bitryssland.py
+++ bitryssland.py
@@ -1,13 +1,8 @@
 input()
-coins = (int(x)for x in input().split())
-products = (int(x)for x in input().split())
+coins = (int(x) for x in input().split())
+products = (int(x) for x in input().split())
 
 balance = sum(count * 2**i for i, count in enumerate(coins))
+price = sum(products)
 
-for price in products:
-    balance -= price
-    if balance < 0:
-        print('nej')
-        break
-else:
-    print('ja' if balance == 0 else 'nej')
+print('ja' if balance-price == 0 else 'nej')
