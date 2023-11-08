7a042cc37796c669b7365cedb93ba2e1f98be08a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 21:09:29 +0100

    breaking if no balance

--- bitryssland.py
+++ bitryssland.py
@@ -6,5 +6,8 @@ balance = sum(count * 2**i for i, count in enumerate(coins))
 
 for price in products:
     balance -= price
+    if balance < 0:
+        print('nej')
+        break
 else:
     print('ja' if balance == 0 else 'nej')
