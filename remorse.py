7924690f1adea74fa163144792899dea4790a029
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 14:00:37 +0100

    include all 0s

--- remorse.py
+++ remorse.py
@@ -1,10 +1,15 @@
 src = tuple(c.upper() for c in input() if c.isalnum())
-chars = reversed(sorted(set(src), key=src.count))
+chars = tuple(reversed(sorted(set(src), key=src.count)))
 
-b, res = 0, -3
-for c in chars:
+b, i, res = 0, 0, -3
+
+while i < len(chars):
     m = format(b, 'b')
     res += (3 + m.count('0') + m.count('1')*3 + len(m)-1) * src.count(chars[i])
+    if m.count('0') == 0:
+        i += 1
+        res += (3 + len(m)+1 + len(m)) * src.count(chars[i])
     b += 1
+    i += 1
 
 print(res)
