fc33361f85566422c4f8baef4a99b4ebf9d02f48
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-08 21:26:12 +0100

    simplify coin conversion

--- bitryssland.py
+++ bitryssland.py
@@ -2,7 +2,7 @@ input()
 coins = (int(x) for x in input().split())
 products = (int(x) for x in input().split())
 
-balance = sum(count * 2**i for i, count in enumerate(coins))
+balance = sum(count << i for i, count in enumerate(coins))
 price = sum(products)
 
 print('ja' if balance-price == 0 else 'nej')
