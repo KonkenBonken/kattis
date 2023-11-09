e1f3d3a995d10d53b337d1db4ff95a20ea5a6889
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-09 18:12:49 +0100

    bfs using a deque

--- ekorren.py
+++ ekorren.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 node_count, nut_count = (int(x) for x in input().split())
 nut_locations = [int(x)-1 for x in input().split()]
 
@@ -7,3 +9,15 @@ for _ in range(node_count-1):
     a, b = (int(x)-1 for x in input().split())
     tree[a][1].append(b)
     tree[b][1].append(a)
+
+curr = tree[0]
+dist = 0
+while len(nut_locations) > 0:
+    queue = deque([curr])
+    while len(queue) > 0:
+        node = queue.popleft()
+        if node[0] in nut_locations:
+            del nut_locations[nut_locations.index(node[0])]
+            curr = node
+            break
+        queue.extend(tree[i] for i in node[1])
