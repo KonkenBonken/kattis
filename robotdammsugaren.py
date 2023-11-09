5fdc49ad3bca1c4a5b9cb6dabebad399ebbb3124
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-09 18:30:08 +0100

    sideways movement

--- robotdammsugaren.py
+++ robotdammsugaren.py
@@ -4,3 +4,11 @@ room = [input() for _ in range(R)]
 
 ry = next(i for i, row in enumerate(room) if 'O' in row)
 rx = next(i for i, tile in enumerate(room[ry]) if 'O' == tile)
+
+for com in sequence:
+    if com == '>':
+        rx = next(i+rx-1 for i,
+                  tile in enumerate(room[ry][rx:]) if '#' == tile)
+    elif com == '<':
+        rx = next(i+1 for i, tile in reversed(
+            list(enumerate(room[ry][:rx+1]))) if '#' == tile)
