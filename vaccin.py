c9b7a90d10cc05ba51f6eb5d1eeef9bb4712c6fb
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 17:11:17 +0100

    slow working solution

---
+++ vaccin.py
@@ -0,0 +1,13 @@
+N, Q = (int(x) for x in input().split())
+days = [int(x) for x in input().split()]
+people = [int(x) for x in input().split()]
+
+for j, queue in enumerate(people):
+    queue += 1
+    for i, vaccins in enumerate(days):
+        queue -= vaccins
+        if queue <= 0:
+            print(i+1)
+            break
+    else:
+        print(-1)
