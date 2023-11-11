8da4e9d498fb105600b80a07272743854ba4b354
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 17:24:52 +0100

    only sum once

--- badstrand.py
+++ badstrand.py
@@ -1,16 +1,19 @@
 plot_count, budget = (int(x) for x in input().split())
 plots = [int(x) for x in input().split()]
+start_price = sum(plots)
 
 
 def main():
     for l in range(plot_count, 0, -1):
-        price = sum(plots[:l])
+        global start_price
+        price = start_price
         for i in range(plot_count-l+1):
             if price <= budget:
                 print(l)
                 return
             if i < plot_count-l:
                 price += plots[i+l] - plots[i]
+        start_price -= plots[l-1]
 
 
 main()
