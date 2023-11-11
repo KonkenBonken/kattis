71b363152c9f93bebd96295096d46e4b89402646
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 16:38:05 +0100

    track price instead calculate sum every iteration
    This reverts commit fe25845e4c7e79c3c9f50593b49476b977c4070e.

--- badstrand.py
+++ badstrand.py
@@ -2,19 +2,15 @@ plot_count, budget = (int(x) for x in input().split())
 plots = [int(x) for x in input().split()]
 
 
-def total(start, length):
-    sum = 0
-    for i in range(length):
-        sum += plots[start+i]
-    return sum
-
-
 def main():
-    for l in range(plot_count+1, 0, -1):
+    for l in range(plot_count, 0, -1):
+        price = sum(plots[:l])
         for i in range(plot_count-l+1):
-            if total(i, l) <= budget:
+            if price <= budget:
                 print(l)
                 return
+            if i < plot_count-l:
+                price += plots[i+l] - plots[i]
 
 
 main()
