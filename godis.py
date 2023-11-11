35fcbb60729b05422214412f4e3dced51533e459
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 15:37:17 +0100

    populate bags with individuals candies

--- godis.py
+++ godis.py
@@ -1,11 +1,21 @@
 N = int(input())
 bags = []
+
 for _ in range(N):
     k, *c = (int(x) for x in input().split())
     bags.append(tuple(c[2*i:2*i+2] for i in range(k)))
 
 combinations = []
 
+
+def flatten(l):
+    flat = []
+    for bag in l:
+        flat.extend(bag)
+    return flat
+
 for length in range(1, N+1):
     for i in range(N-length+1):
-        combinations.append(bags[i:i+length])
+        comb = flatten(flatten([[s]*n for s, n in bag]
+                               for bag in bags[i:i+length]))
+        combinations.append(comb)
