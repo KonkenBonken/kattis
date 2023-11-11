6bf3c6eaa65d55ed12c34c82c547aa9497954b5a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 16:08:44 +0100

    replace main function with exit()

--- badstrand.py
+++ badstrand.py
@@ -1,13 +1,8 @@
 N, B = (int(x) for x in input().split())
 A = [int(x) for x in input().split()]
 
-
-def main():
-    for l in range(N+1, 0, -1):
-        for i in range(N-l+1):
-            if sum(A[i:i+l]) <= B:
-                print(l)
-                return
-
-
-main()
+for l in range(N+1, 0, -1):
+    for i in range(N-l+1):
+        if sum(A[i:i+l]) <= B:
+            print(l)
+            exit()
