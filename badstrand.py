97d6394666a780e0f3273ec679269c37307445c0
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-11 16:16:52 +0100

    reintroduce main function
    This reverts commit 6bf3c6eaa65d55ed12c34c82c547aa9497954b5a.

--- badstrand.py
+++ badstrand.py
@@ -1,8 +1,13 @@
 plot_count, budget = (int(x) for x in input().split())
 plots = [int(x) for x in input().split()]
 
-for l in range(plot_count+1, 0, -1):
-    for i in range(plot_count-l+1):
-        if sum(plots[i:i+l]) <= budget:
-            print(l)
-            exit()
+
+def main():
+    for l in range(plot_count+1, 0, -1):
+        for i in range(plot_count-l+1):
+            if sum(plots[i:i+l]) <= budget:
+                print(l)
+                return
+
+
+main()
