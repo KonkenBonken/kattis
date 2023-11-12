531814fdd6d582b0a1777c0597024a0cee4db332
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-12 21:12:44 +0100

    refactor variables

--- dingdongditch.py
+++ dingdongditch.py
@@ -1,6 +1,6 @@
-N, Q = (int(x) for x in input().split())
-A = sorted(int(x) for x in input().split())
-B = (int(x) for x in input().split())
+input()
+houses = sorted(int(x) for x in input().split())
+friends = (int(x) for x in input().split())
 
-for b in B:
-    print(sum(A[:b]))
+for count in friends:
+    print(sum(houses[:count]))
