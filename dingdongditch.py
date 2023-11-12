a231e0b71fb8f4f0e0eef4bcfaa761fabc618b0a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-12 21:31:05 +0100

    calculate cumulatively

--- dingdongditch.py
+++ dingdongditch.py
@@ -1,6 +1,15 @@
 input()
 houses = sorted(int(x) for x in input().split())
-friends = (int(x) for x in input().split())
 
-for count in friends:
-    print(sum(houses[:count]))
+friends = sorted(((i, int(b))
+                 for i, b in enumerate(input().split())), key=lambda v: v[1])
+
+total, last = 0, 0
+
+for i, friend in enumerate(friends):
+    j, count = friend
+    total += sum(houses[last:count])
+    last = count
+    friends[i] = j, total
+
+print(*[x for _, x in sorted(friends, key=lambda v: v[0])], sep='\n')
