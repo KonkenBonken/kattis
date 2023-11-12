39e5e809cc25b3c9352381da30440313fb3962d1
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-12 20:53:21 +0100

    attempt on group 2

--- sagverket.py
+++ sagverket.py
@@ -1,7 +1,19 @@
-# Group 1: N=1
+# Group 1&2: N=1|2
 N, T = (int(x) for x in input().split())
 
 start_num = 10**9
 print('?', start_num)
-response = int(input())
-print('!', start_num - response)
+
+if T == 1:
+    response = int(input())
+    print('!', start_num - response)
+elif T == 2:
+    response = [int(x) for x in input().split()]
+    x = start_num-response[0]
+    print('?', x)
+    print(response)
+    response = [int(x) for x in input().split()]
+    print(response)
+
+    y = start_num-response[0]
+    print('!', x, y)
