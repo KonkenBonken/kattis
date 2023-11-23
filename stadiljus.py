f97f7fbc6b331a3743c3d35bf69c87af4e6240f1
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-23 18:30:06 +0100

    solve group 1 & 2

--- stadiljus.py
+++ stadiljus.py
@@ -1,2 +1,14 @@
-N, X, Y = int(input()), int(input()), int(input())
-L = [int(x) for x in input().split()]
+N, price, max_mean = int(input()), int(input()), int(input())
+places = [int(x)*price for x in input().split()]
+
+places.sort()
+sum = 0
+
+for i, place in enumerate(places):
+    if (sum+place)//(i+1) <= max_mean:
+        sum += place
+    else:
+        print(i)
+        break
+else:
+    print(N)
