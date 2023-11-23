7186fb51b9f59b49fa1723044fa54fbc45ddc47a
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-23 19:02:03 +0100

    discard too long subsequences

--- proteinsyntes.py
+++ proteinsyntes.py
@@ -5,6 +5,7 @@ T = input()
 proteins = sorted((input()
                   for _ in range(int(input()))), key=len, reverse=True)
 
+L = len(T)
 queue = deque((p, 1) for p in proteins)
 
 while True:
@@ -12,4 +13,4 @@ while True:
     if p == T:
         print(i)
         break
-    queue.extend((p+q, i+1) for q in proteins if p+q in T)
+    queue.extend((p+q, i+1) for q in proteins if p+q in T and len(p+q) <= L)
