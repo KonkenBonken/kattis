470715c3b63886ad03ed812d41f2a540d7c7362e
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-24 20:28:47 +0100

    slow solution using bfs deque

--- trafikverketsplan.py
+++ trafikverketsplan.py
@@ -1,7 +1,25 @@
+from collections import deque
+
 N = int(input())
 houses = tuple([] for _ in range(N))
 
 for _ in range(N-1):
     a, b = (int(x)-1 for x in input().split())
-    print(a)
     houses[a].append(b)
+
+Q = int(input())
+
+for _ in range(Q):
+    h, w = (int(x)-1 for x in input().split())
+
+    queue = deque([h])
+    visited = set()
+    while len(queue) > 0:
+        house = queue.popleft()
+        if house == w:
+            print('ja')
+            break
+        if house not in visited:
+            queue.extend(houses[house])
+    else:
+        print('nej')
