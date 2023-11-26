f58447b88e6f49037e864f0da16155ec4662431c
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-26 17:13:09 +0100

    only run min() or max() if needed

--- segmentovervakning.py
+++ segmentovervakning.py
@@ -3,6 +3,7 @@ Q = int(input())
 
 l_cams, u_cams, f_cams = dict(), dict(), set()
 l_max, u_min = 0, 10**9
+l_chg, u_chg = False, False
 
 for _ in range(Q):
     sign, s, *n = input().split()
@@ -13,26 +14,32 @@ for _ in range(Q):
             f_cams.add(s)
         elif a <= Min and b >= Min:
             l_cams[s] = b
-            l_new = True
+            l_chg = True
         elif a <= Max and b >= Max:
             u_cams[s] = a
-            u_new = True
+            u_chg = True
     else:
         if l_cams.pop(s, -1) == -1:
             if u_cams.pop(s, -1) == -1:
                 f_cams.discard(s)
+            else:
+                u_chg = True
+        else:
+            l_chg = True
 
     if len(f_cams) != 0:
         print(1)
         continue
+    elif len(l_cams) == 0 or len(u_cams) == 0:
+        print(-1)
+        continue
 
-    if l_new:
+    if l_chg:
         l_max = max(l_cams.values())
-    elif u_new:
+    if u_chg:
         u_min = min(u_cams.values())
 
-    if len(l_cams) != 0 and len(u_cams) != 0:
-        if l_max >= u_min:
-            print(2)
-            continue
-    print(-1)
+    if l_max >= u_min:
+        print(2)
+    else:
+        print(-1)
