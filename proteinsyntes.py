2b9c034750f8444b5a0fd37e0481f9f50f93719e
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-26 18:06:34 +0100

    append subqueues instead of values

--- proteinsyntes.py
+++ proteinsyntes.py
@@ -6,13 +6,17 @@ proteins = sorted((p for p in (input()
                   for _ in range(int(input()))) if p in T), key=len, reverse=True)
 
 L, M = len(T), min(len(p) for p in proteins)
-queue = deque((p, 1) for p in proteins)
+queue = deque([(1, proteins)])
 
-while True:
-    p, i = queue.popleft()
-    if p == T:
-        print(i)
-        break
-    if len(p) <= L-M:
-        queue.extend((p+q, i+1)
-                     for q in proteins if p+q in T and len(p+q) <= L)
+while len(queue) > 0:
+    i, subqueue = queue.popleft()
+    for p in subqueue:
+        if p == T:
+            print(i)
+            break
+        if len(p) <= L-M:
+            queue.append(
+                (i+1, [p+q for q in proteins if p+q in T and len(p+q) <= L]))
+    else:
+        continue
+    break
