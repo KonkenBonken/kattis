a1f0b0d35c9b79181f468f4da6a16f3a43be2f08
KonkenBonken <konrad05pettersson@gmail.com>
2023-11-26 16:35:07 +0100

    avoid min() and max()

--- segmentovervakning.py
+++ segmentovervakning.py
@@ -2,10 +2,12 @@ Min, Max = (int(x) for x in input().split())
 Q = int(input())
 
 l_cams, u_cams, f_cams = dict(), dict(), set()
+l_max, u_min = 0, 10**9
 
 for _ in range(Q):
     sign, *n = input().split()
     n = tuple(int(x) for x in n)
+    l_new, u_new = False, False
 
     if sign == '+':
         s, a, b = n
@@ -13,8 +15,10 @@ for _ in range(Q):
             f_cams.add(s)
         elif a <= Min and b >= Min:
             l_cams[s] = b
+            l_new = True
         elif a <= Max and b >= Max:
             u_cams[s] = a
+            u_new = True
     else:
         l_cams.pop(n[0], 0)
         u_cams.pop(n[0], 0)
@@ -22,9 +26,15 @@ for _ in range(Q):
 
     if len(f_cams) != 0:
         print(1)
-    elif len(l_cams) == 0 or len(u_cams) == 0:
-        print(-1)
-    elif max(l_cams.values()) >= min(u_cams.values()):
-        print(2)
-    else:
-        print(-1)
+        continue
+
+    if l_new:
+        l_max = max(l_cams.values())
+    elif u_new:
+        u_min = min(u_cams.values())
+
+    if len(l_cams) != 0 and len(u_cams) != 0:
+        if l_max >= u_min:
+            print(2)
+            continue
+    print(-1)
