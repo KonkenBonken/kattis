6c3d8ecb3248700fada84603b56bf12fcfaeee36
KonkenBonken <konrad05pettersson@gmail.com>
2024-01-10 18:56:31 +0100

    loop over Jill instead of set intersection

--- cd.py
+++ cd.py
@@ -1,4 +1,11 @@
-jack, jill = (
-    set(int(input()) for _ in range(int(x))) for x in input().split())
-
-print(len(jack & jill))
+while True:
+    N, M = (int(x) for x in input().split())
+    if N == 0 and M == 0:
+        break
+    jack = set(input() for _ in range(N))
+    jill = (input() for _ in range(M))
+    CDs = 0
+    for cd in jill:
+        if cd in jack:
+            CDs += 1
+    print(CDs)
