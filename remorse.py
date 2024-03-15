801285f0c585e8404ed0f6f815984736fe224a88
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-16 00:40:26 +0100

    precompute morse characters

--- remorse.py
+++ remorse.py
@@ -1,20 +1,25 @@
+from math import log2, ceil
+
 src = tuple(c.upper() for c in input() if c.isalpha())
-chars = sorted(set(src), key=src.count)
+chars = sorted(set(src), key=src.count, reverse=True)
 
 if len(src) == 1:
     print(1)
     exit()
 
-res = -2
-l = 0
+codes = []
 
-while True:
-    for b in range(2**l - 1):
+for l in range(1, ceil(log2(len(chars))) + 3):
+    for b in range(2**l):
         m = format(b, 'b').rjust(l, '0')
-        char = m.count('0') + m.count('1')*3 + l-1
-        char *= src.count(chars.pop())
-        res += 3 + char
-        if len(chars) == 0:
-            print(res)
-            exit()
-    l += 1
+        char = m.count('0') + m.count('1')*3 + l-1 + 3
+        codes.append(char)
+
+codes.sort(reverse=True)
+
+res = -3
+
+for char in chars:
+    res += codes.pop()*src.count(char)
+
+print(res)
