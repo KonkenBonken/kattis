21e1856db43a6450c1d36470130040e21b2f5ffc
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-15 23:45:12 +0100

    create custom deque class based on dict

--- teque.py
+++ teque.py
@@ -1,4 +1,34 @@
-from collections import deque
+class deque:
+    def __init__(self):
+        self.a = 1
+        self.b = 0
+        self.d = dict()
+
+    def af(self, num: int):
+        self.a -= 1
+        self.d[self.a] = num
+
+    def ab(self, num: int):
+        self.b += 1
+        self.d[self.b] = num
+
+    def popf(self):
+        v = self.d[self.a]
+        del self.d[self.a]
+        self.a += 1
+        return v
+
+    def popb(self):
+        v = self.d[self.b]
+        del self.d[self.b]
+        self.b -= 1
+        return v
+
+    def __getitem__(self, i: int):
+        return self.d[i]
+
+    def __len__(self):
+        return self.b - self.a + 1
 
 
 left, right = deque(), deque()
@@ -8,21 +38,21 @@ for _ in range(int(input())):
     num = int(num)
 
     if cmd == 'push_back':
-        right.append(num)
+        right.ab(num)
 
     elif cmd == 'push_front':
-        left.appendleft(num)
+        left.af(num)
 
     elif cmd == 'push_middle':
         if (len(left) != len(right)) and (len(left) != len(right) + 1):
 
             while len(left) < len(right):
-                left.append(right.popleft())
+                left.ab(right.popf())
 
             while len(left) > len(right) + 1:
-                right.appendleft(left.pop())
+                right.af(left.popb())
 
-        right.appendleft(num)
+        right.af(num)
 
     elif cmd == 'get':
         if num < len(left):
