79f981956a4386b296376cfd1efcd8b7e44d17ff
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-11 16:08:50 +0200

    first try

---
+++ coast.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+H, W = map(int, input().split())
+
+grid: 'list[tuple[bool,int,int]]' = []
+
+for y in range(H):
+    grid.extend((n == '1', x, y) for x, n in enumerate(input()))
+
+
+def at(x: int, y: int):
+    if x >= 0 and x < W and y >= 0 and y < H:
+        return grid[x+y*W]
+
+
+def neighbors(tile):
+    _, x, y = tile
+    return (t for t in [
+        at(x+1, y),
+        at(x, y+1),
+        at(x-1, y),
+        at(x, y-1),
+    ] if t)
+
+
+queue = deque((l, x, y) for l, x, y in grid
+              if not l and (y == 0 or y == H-1 or x == 0 or x == W-1))
+
+coast = 0
+visited = set()
+
+visual = [[0 for x in range(W)] for y in range(H)]
+
+while len(queue):
+    tile = queue.popleft()
+    neighs = neighbors(tile)
+    print(tile, tuple(neighbors(tile)))
+    for n in neighs:
+        if n[0]:
+            coast += 1
+            visual[tile[2]][tile[1]] += 1
+            print(coast)
+        else:
+            if n not in visited:
+                queue.append(n)
+    visited.add(tile)
+
+print(*visual, sep='\n')
+
+print(coast)
