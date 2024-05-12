8d7ad617b7d9a51e60914e4b0b8a23e50f8a810f
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-12 19:18:42 +0200

    remove visuals

--- coast.py
+++ coast.py
@@ -29,22 +29,15 @@ queue = deque((l, x, y) for l, x, y in grid
 coast = H*2 + W*2 - 4 - len(queue)
 visited = set(queue)
 
-visual = [[0 for x in range(W)] for y in range(H)]
-
 while len(queue):
     tile = queue.popleft()
     neighs = neighbors(tile)
-    print(tile, tuple(neighbors(tile)))
     for n in neighs:
         if n[0]:
             coast += 1
-            visual[tile[2]][tile[1]] += 1
-            print(coast)
         else:
             if n not in visited:
                 queue.append(n)
                 visited.add(n)
-
-print(*visual, sep='\n')
 
 print(coast)
