ad5e01ad5c28ee63b4a1677177d7bda93e842384
KonkenBonken <konrad05pettersson@gmail.com>
2024-05-13 22:38:11 +0200

    kodgolf

--- stokigalistor.py
+++ stokigalistor.py
@@ -1,10 +1,4 @@
 input()
-L = [int(x) for x in input().split()]
-S = sorted(L)
-count = 0
-
-for i, n in enumerate(L):
-    if n != S[i]:
-        count += 1
-
-print(count)
+L=[int(x)for x in input().split()]
+S=sorted(L)
+print(sum(1 for i,n in enumerate(L)if n!=S[i]))
