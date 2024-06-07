82ad255da303b892cbb36bc94b677d0bb6b00edf
KonkenBonken <konrad05pettersson@gmail.com>
2024-06-07 23:43:15 +0200

    reduce max length

--- aliennumbers.jl
+++ aliennumbers.jl
@@ -2,7 +2,7 @@ for caseNum in 1:parse(UInt8, readline())
   ali, src, trg = split(readline())
 
   num = reduce((tot, (i, c)) -> tot + (findfirst(c, src) - 1) * length(src)^(i - 1), enumerate(ali[end:-1:1]); init=0)
-  reslen = findfirst(i -> num < length(trg)^(i + 1), 0:10^10)
+  reslen = findfirst(i -> num < length(trg)^(i + 1), 0:94)
 
   function digit(i)
     div = num ÷ length(trg)^i + 1
