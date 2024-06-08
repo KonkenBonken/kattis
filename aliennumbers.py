41b0c84e073aa75ff0d9cf1fe4c15a69a0f381ce
KonkenBonken <konrad05pettersson@gmail.com>
2024-06-08 16:17:51 +0200

    use log for reslen

--- aliennumbers.py
+++ aliennumbers.py
@@ -1,11 +1,13 @@
+from math import floor, log
+
 for case in range(int(input())):
     ali, src, trg = input().split()
     res = ''
 
     num = sum(src.index(c) * len(src)**i for i, c in enumerate(reversed(ali)))
-    strt = next(i for i in range(94) if num < len(trg)**i)
-        
-    for i in range(strt-1, -1, -1):
+    reslen = floor(log(num, len(trg))) + 1
+
+    for i in range(reslen-1, -1, -1):
         div, num = divmod(num, len(trg)**i)
         res += trg[div]
 
